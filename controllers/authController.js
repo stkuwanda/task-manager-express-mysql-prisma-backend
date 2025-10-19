ccd4ae5e7a5ae340961092cd5c4847d97284b4e7
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-10-19 06:28:49 +0200

    implement login api and login testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import { prismaClient } from '../routes/index.js';
-import { hashSync } from 'bcrypt';
+import { compareSync, hashSync } from 'bcrypt';
 
 export const RegisterController = async (req, res) => {
 	try {
@@ -28,6 +28,38 @@ export const RegisterController = async (req, res) => {
 		});
 
 		res.status(201).json({ message: 'User registered successfully.', user });
+	} catch (err) {
+		res.status(500).json({ error: { message: 'Internal server error.' } });
+	}
+};
+
+export const LoginController = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		if (!email || !password) {
+			return res
+				.status(400)
+				.json({ error: { message: 'Missing credentials!' } });
+		}
+
+		const existingUser = await prismaClient.user.findUnique({
+			where: { email },
+		});
+
+		if (!existingUser) {
+			return res.status(404).json({ error: { message: 'Not found!' } });
+		}
+
+		const isPasswordValid = compareSync(password, existingUser.password);
+
+		if (!isPasswordValid) {
+			return res
+				.status(401)
+				.json({ error: { message: 'Invalid credentials!' } });
+		}
+
+		res.status(200).json({ message: 'Login successful.', user: existingUser });
 	} catch (err) {
 		res.status(500).json({ error: { message: 'Internal server error.' } });
 	}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { RegisterController } from '../controllers/authController.js';
+import { LoginController, RegisterController } from '../controllers/authController.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', RegisterController);
+
+authRouter.post('/login', LoginController);
 
 export default authRouter;
